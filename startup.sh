42938be4d69b62cac6c1f55b83d7319c84c7fc96
LeoBruant <bruantleo@gmail.com>
2024-05-03 23:10:30 +0200

    Feat : Update
    Signed-off-by: LeoBruant <bruantleo@gmail.com>

--- startup.sh
+++ startup.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-sudo apt install git nodejs -y
+# Node
+sudo apt install -y git nodejs
 
-git clone https://github.com/LeoBruant/terraform-project && cd terraform-project
+# Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+
+# Start app
+git clone https://github.com/LeoBruant/terraform-project 
+
+cd terraform-project
+
+npm ci
 npm run build
+
+sg docker -c "docker compose up -d"
 
 exit 0
